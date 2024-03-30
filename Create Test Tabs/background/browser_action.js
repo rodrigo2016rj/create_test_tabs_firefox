f1244fd2bcaff50e2bd7f1e39ee28d8e26157c5f
Rodrigo Diniz da Silva <rodrigo_diniz_da_silva@yahoo.com>
2024-03-30 11:30:06 -0300

    Atualizei para que este addon funcione com a versão 3 do arquivo manifest.json

--- Create Test Tabs/background/browser_action.js
+++ Create Test Tabs/background/browser_action.js
@@ -1,4 +1,4 @@
-browser.browserAction.onClicked.addListener(function(aba){
+browser.action.onClicked.addListener(function(aba){
   var propriedades_da_nova_aba = {};
   propriedades_da_nova_aba.url = "background/painel_do_create_test_tabs.html";
   browser.tabs.create(propriedades_da_nova_aba);
